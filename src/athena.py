b5327a2e96c114244656fa0d83e7a9e24763a575
__SYN__ <aero.sayan@gmail.com>
2018-07-17 13:38:45 +0530

    Overhauled line generation system by implementing a parser
    PROGRESS:
    + Implemented a simple parser to get commands from comma delimited string
        - Can set allignment
        - Can set font color
        - Can set font size
    TODO:
    + Allow selection of rendering position through same parser
    
    Signed-off-by: __SYN__ <aero.sayan@gmail.com>

--- src/athena.py
+++ src/athena.py
@@ -3,8 +3,7 @@
 # AUTH : Sayan Bhattacharjee
 # EMAIL: aero.sayan@gmail.com
 # DATE : 16/JULY/2018            (Started Creation  )
-# DATE : 00/XXXX/XXXX            (Finished Creation )
-# DATE : 16/JULY/2018            (Last Modified     )
+# DATE : 17/JULY/2018            (Last Modified     )
 # INFO : Athena - Library for creating educational video frames in LaTex format
 from __future__ import print_function
 import matplotlib.pyplot as plt
@@ -32,7 +31,9 @@ class Page:
         # The Athena core data structure
         #------------------------------------------------------
         self.lines = [] # Lines to be rendered in 2D matrix form
-        self.allignment = [] # Allignment of the lines in 2D matrix form
+        self.allign= [] # Allignment of the lines in 2D matrix form
+        self.font_size = [] # Font size of the lines
+        self.font_color = [] # Font color of the lines
 
     def __draw_call(self,dpi,bgcolor='black',tofile=False):
         """ The final draw call that will render the page
@@ -61,7 +62,7 @@ class Page:
         @param toFile : if true then print to file else render on screen
         """
         # Sanity check
-        if len(self.lines) != len(self.allignment):
+        if len(self.lines) != len(self.allign):
             print("ERR : Page can not be rendered...")
             print("ERR : No. of lines and allignment mismatch...")
             assert(False)
@@ -69,41 +70,51 @@ class Page:
         fig = plt.figure(facecolor=athena.bg_color ,\
         figsize=[athena.length/athena.dpi, athena.width/athena.dpi],\
         dpi = athena.dpi)
+
         # Set the color parameters TODO : make it controllable
         plt.rcParams['figure.figsize'] =[athena.length/athena.dpi, athena.width/athena.dpi]
         plt.rcParams['figure.facecolor'] = athena.bg_color
         plt.rcParams['axes.facecolor'] = athena.bg_color
         plt.rcParams['text.color'] = athena.font_color
+        # Delete axes
+        #fig.axes.get_xaxis().set_visible(False)
+        #fig.axes.get_yaxis().set_visible(False)
+        plt.axis('off')
         # Clear figure
         plt.gcf()
+        print(len(self.lines))
         for i in xrange(len(self.lines)):
             # Render line
-            self.render_line(i,athena.font_size)
+            self.render_line(i)
             # Update rendering position
             self.__update_ypos()
         # Call final draw call for this page
         self.__draw_call(athena.dpi,athena.bg_color,tofile)
 
 
-    def render_line(self,line_num,fontsize):
+    def render_line(self,line_num):
         """ Render one line after aligning it correctly and sizing it correctly
         @param line_num : the position of the line in self.lines to be printed
         """
         plt.text(self.xpos_curr,self.ypos_curr,self.lines[line_num],\
-        fontsize=fontsize,ha=self.allignment[line_num])
+        fontsize=self.font_size[line_num],ha=self.allign[line_num],\
+        color=self.font_color[line_num])
 
-    def add_line(self,line,allign='left'):
+
+    def add_line(self,cmd):
         """ Add another string of text to the page
-        @line : string to be added to the page and later to be rendered
+        @cmd : command for string to be rendered onto the string
         """
-        self.lines.append(line)
-        self.allignment.append(allign)
+        self.lines.append(cmd[0][0]) # text
+        self.allign.append(cmd[1][0]) # allignment
+        self.font_size.append(cmd[2][0]) # font size
+        self.font_color.append(cmd[3][0]) # font color
 
 
 
 class Athena:
     """ The base class for creating educational video frames in LaTex format """
-    def __init__(self,length,width,dpi,bgcolor='black',fontcolor='white'):
+    def __init__(self,length,width,dpi,bgcolor='#011012',fontcolor='white'):
         """ Constructor
         @param length : the length in pixels of the image to be rendered
         @param width : the width in pixels of the image to be rendered
@@ -121,7 +132,7 @@ class Athena:
         # Background color,font color and font size to use in the rendering
         self.bg_color = bgcolor
         self.font_color = fontcolor
-        self.font_size = 30
+        self.font_size = 30.0
         #------------------------------------------------------
         # Parameters set by default
         #------------------------------------------------------
@@ -130,14 +141,99 @@ class Athena:
         # The Athena core data structures
         #------------------------------------------------------
         self.pages = [] # NOTE : self.text will be an array of object of Page
+    def parse_command(self,cmdstr,echo=False):
+        """ Parse the commands given in the command string and return them
+        @param cmdstr : comma delimited command string
+        @info  cmdstr : "t,$\mathtt{EULER'S-IDENTITY}$,a,center,s,30,c,#1111"
+        @info  t : text then the text to be printed
+        @info  a : allignment then the allignment to be assigned
+        @info  s : font size then the fontsize to be assigned
+        @info  c : color then the color to be assigned (probably in hex)
+        """
+        # Split all the commands by comma
+        cmds = cmdstr.split(',')
 
-    def create_page(self,data):
+        # Prepare arrays for capturing
+        arr_t = []
+        arr_a = []
+        arr_s = []
+        arr_c = []
+
+        # Set some flags for capturing
+        cap_t = False     # Capture text
+        cap_a = False     # Capture allignment
+        cap_s = False     # Capture font size
+        cap_c = False     # Capture color
+        # Go through the text and find all data
+        for c in cmds:
+            if c == 't':
+                cap_t = True
+                continue
+            elif c == 'a' :
+                cap_a = True
+                continue
+            elif c == 's':
+                cap_s = True
+                continue
+            elif c == 'c':
+                cap_c = True
+                continue
+            elif cap_t == True:
+                arr_t.append(c)
+                cap_t = False
+                continue
+            elif cap_a == True:
+                arr_a.append(c)
+                cap_a = False
+                continue
+            elif cap_s == True:
+                arr_s.append(float(c))
+                cap_s = False
+                continue
+            elif cap_c == True:
+                arr_c.append(c)
+                cap_c = False
+                continue
+        # Ensure if data is not set by user then to set them to default values
+        if arr_t == []:
+            # If text is not found then show an error
+            print("ERR : arr_t is [] : meaning no text is found...")
+            print("ERR : Can not render without text")
+            assert(False)
+        if arr_a == []:
+            # If allignment is empty then replace with default allignment
+            arr_a.append("left")
+        if arr_s == []:
+            # If size is empty then replace with default size
+            arr_s.append(self.font_size)
+        if arr_c == []:
+            # If color is empty then replace with default text color
+            arr_c.append(self.font_color)
+
+        if echo : print("DBG : Parsed commands"); print(arr_t,arr_a,arr_s,arr_c)
+        return (arr_t,arr_a,arr_s,arr_c)
+
+
+
+
+    def create_page(self,cmdstr):
         """ Create page and add to Athena
-        @param data : get both the line and the allignment data
-        @info  data = [["hello"],["world","left"]]
+        @param cmdstr : comma delimited command string
+        @info  cmdstr : "t,$\mathtt{EULER'S-IDENTITY}$,a,center,s,30,c,#1111"
+        @info  t : text then the text to be printed
+        @info  a : allignment then the allignment to be assigned
+        @info  s : font size then the fontsize to be assigned
+        @info  c : color then the color to be assigned (probably in hex)
         """
         # Create the page object
         page = Page()
+        cmds = []
+        # Iterate through every line command given parse it then add to page
+        for c in cmdstr:
+            cmds.append(self.parse_command(c,echo=True))
+            page.add_line(cmds[-1])
+
+        """
         # Iterate through every line given
         for i in xrange(len(data)):
             if len(data[i]) == 1 :
@@ -149,13 +245,12 @@ class Athena:
             else:
                 print("ERR : lines_alligned size error...")
                 assert(False)
-
+        """
         # Append the newly created page to self.pages
         self.pages.append(page)
         print(self.pages[-1])
         print(self.pages[-1].lines)
-        print(self.pages[-1].allignment)
-
+        print(self.pages[-1].allign)
 
 
 
@@ -169,6 +264,7 @@ class Athena:
         @param n : the position of the page to be rendered
         """
         self.pages[n].render_page(self,tofile=tofile)
+
 
     def render(self,toFile=False):
         """ Render every page to either screen or file
--- src/test_athena.py
+++ src/test_athena.py
@@ -4,8 +4,7 @@ import athena as at
 # AUTH : Sayan Bhattacharjee
 # EMAIL: aero.sayan@gmail.com
 # DATE : 16/JULY/2018            (Started Creation  )
-# DATE : 00/XXXX/XXXX            (Finished Creation )
-# DATE : 16/JULY/2018            (Last Modified     )
+# DATE : 12/JULY/2018            (Last Modified     )
 # INFO : Test the athena library
 if __name__ == "__main__":
     length = 1080 # length of the image in pixels
@@ -14,11 +13,23 @@ if __name__ == "__main__":
     ath = at.Athena(length,width,dpi) # create athena object
     # Get data for a page
     page1 = [] # NOTE : 2D structure is required
-    line1 = ["$\mathtt{EULER'S-IDENTITY}$","center"]
-    line2 = ["$e^{i\pi} + 1 = 0$","center"] # Store both the line text and allignment
+
+    # Test 1: Euler's Identity
+    #line1 = "t,$\mathtt{EULER'S-IDENTITY}$,a,center,s,40,c,#e85f04"
+    #line2 = "t,$e^{i\pi} + 1 = 0$,a,center,s,27"
+
+    # Test 2 : Navier Stoke's Equation
+    line1 = "t,$\mathtt{NAVIER-STOKE'S-EQUATION}$,a,center,s,50,c,#90de1a"
+    line2 = "t,$\\rho \\left[ \\frac{\delta \\vec u}{\delta t} + "\
+    +"\\vec u . \\nabla \\vec u \\right] = - \\nabla p + \\nu \Delta \\vec u $,"+\
+    "a,center,s,25"
+    line3 = "t,$\\nabla . \\vec u = 0$,a,center,s,25"
+
     page1.append(line1) # NOTE : Creates a 2D structure which is required
     page1.append(line2)
+    page1.append(line3)
     # Create a page using the data
     ath.create_page(page1)
     # Render the page
-    ath.render_page_at(0,tofile=True)
+    ath.render_page_at(0,tofile=False)
+    #ath.parse_command("t,$\mathtt{EULER'S-IDENTITY}$,a,center,s,40,c,red",echo=True)
